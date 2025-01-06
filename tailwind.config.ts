089f0300710f2d66ba5bbc269b759c003adb7d1f
akuasli <cruzzadersreal@gmail.com>
2025-01-06 12:08:47 +0700

    fum

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-export default {
+const config: Config = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
-} satisfies Config;
+});
+
+export default config;
